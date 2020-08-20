a5d46a4c7004eaa86e2fbc4c81d47bd1df67682a
vihndsm <67498073+vihndsm@users.noreply.github.com>
2020-08-20 13:02:32 +0600

    Create OOP.py
    master

---
+++ OOP.py
@@ -0,0 +1,31 @@
+# class Person:
+#     name = 'default'
+#     age = '18'
+#     prof = 'IT'
+
+#     def set(self, name, age, prof):
+#         self.name = name
+#         self.age = age
+#         self.prof = prof
+
+# aito = Person()
+# aito.set('Aito', 18, 'PY dev')
+# print(aito.name, aito.age, aito.prof)
+
+# taalai = Person()
+# taalai.name = 'Taalai'
+# taalai.age = 23
+# taalai.prof = 'dev'
+# print(taalai.name, taalai.age, taalai.prof)
+
+# turat = Person()
+# turat.name = 'Turatbek'
+# turat.age = 30
+# turat.prof = 'juni dev'
+# print(turat.name, turat.age, turat.prof)
+
+# umut = Person()
+# umut.name = 'Umut'
+# # umut.age = 29
+# # umut.prof = 'mid dev'
+# print(umut.name, umut.age, umut.prof)
