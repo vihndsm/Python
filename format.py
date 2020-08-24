4b80a636da451f1e41d11622c71d534af511a8fe
vihndsm <67498073+vihndsm@users.noreply.github.com>
2020-08-24 12:48:31 +0600

    master

---
+++ format.py
@@ -0,0 +1,12 @@
+# name = 'Trump'
+# lastname = 'Donald'
+# time = '15:00'
+
+# # sms = 'missed from:' + name + lastname + ' at ' + time + ' pess 1 to listen voice message'
+# # print(sms)
+
+# sms = 'missed call from: {1} {0}  at  {2}  pess go to listen voice message' . format(name, lastname, time)
+# print(sms)
+
+# name = ['Turat', 'Kiril', 'Taalai', 'Umut']
+# print('hi {0[0]} \nhello {0[1]} \nпривет {0[2]} \nсалам {0[3]}'.format(name))
