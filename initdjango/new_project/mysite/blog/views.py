5f5c1a7302bf1bb90d49077d34f5dcd28e9e480f
vihndsm <67498073+vihndsm@users.noreply.github.com>
2020-09-18 12:24:32 +0600

    master

--- initdjango/new_project/mysite/blog/views.py
+++ initdjango/new_project/mysite/blog/views.py
@@ -1,9 +1,50 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+def post_share(request, post_id):
+    # Получение статьи по идентификатору.
+    post = get_object_or_404(Post, id=post_id,status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+        # Все поля формы прошли валидацию.
+            cd = form.cleaned_data
+        # ... Отправка электронной почты.
+        post_url = request.build_absolute_uri(post.get_absolute_url())
+        subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+        message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+        send_mail(subject, message, 'vihndsm@gmail.com', [cd['to']])
+        sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+# def post_list(request):
+#     # posts = Post.published.all()
+#     # return render(request, 'blog/post/list.html', {'posts': posts})
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3) # По 3 статьи на каждой странице. 
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#     # Если страница не является целым числом, возвращаем первую страницу.
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#     # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request,'blog/post/list.html', {'page': page, 'posts': posts})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',publish__year=year, publish__month=month, publish__day=day) 
