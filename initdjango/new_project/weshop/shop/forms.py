83fbba732ac6936d8e2c11f9a503757fd448e6e4
admin <admin@admins-MacBook-Pro.local>
2020-10-14 12:29:38 +0600

    ьфыеук

--- initdjango/new_project/weshop/shop/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Applications
-from django.db import models
-
-
-class ApplicationsForm(forms.ModelForm):
-    class Meta:
-        model = Applications
-        fields = ['mail', 'name', 'comment']
